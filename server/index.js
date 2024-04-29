3b77fbace0ddefaa3bf12e381c57a4dfef40b07f
Govind Penshanwar <govindpenshanwar32@gmail.com>
2024-04-29 15:27:48 +0530

    fixed issues

--- server/index.js
+++ server/index.js
@@ -122,6 +122,9 @@ app.post("/loginData", async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
+            expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+            sameSite: "None",
+            secure: true,
         });
         return res.status(200).json({
             message: "Login Successfull",
