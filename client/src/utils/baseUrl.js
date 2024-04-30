1a1e5ec8bf5bbc9369123eee5656d86f65908cd8
Govind Penshanwar <govindpenshanwar32@gmail.com>
2024-04-30 18:26:49 +0530

    added vercel.json file

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,4 +1,5 @@
-const baseUrl = "https://blog-app-7dlf.onrender.com";
+// const baseUrl = "https://blog-app-7dlf.onrender.com";
+const baseUrl = "https://blog-app-backend-blond.vercel.app";
 // const baseUrl = "http://localhost:4000";
 
 export default baseUrl;
